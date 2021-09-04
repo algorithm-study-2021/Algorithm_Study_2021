d0e0c21ffda41766f62bb98e575de9b5b9f62557
YJ KIM <81635224+Yjee0215@users.noreply.github.com>
2021-09-04 10:49:25 +0900

    Update last_common_multiple.py

--- 김유진/1week/last_common_multiple.py
+++ 김유진/1week/last_common_multiple.py
@@ -1,12 +1,17 @@
-from math import gcd
+def gcd(a,b):
+    while b!=0:
+        r=a%b
+        a=b
+        b=r
+    return a
 
 def solution(arr):
     
     for i in range(len(arr)-1):
         temp_gcd=gcd(arr[i],arr[i+1])
         #최소공배수 구하기
-        arr[i+1]=int((arr[i]*arr[i+1])/temp_gcd)       
+        arr[i+1]=(arr[i]*arr[i+1])/temp_gcd
+        print(arr)        
+    
 
-    return arr[len(arr)-1] 
-
-print(solution([2,6,8,14]))
+    return arr[len(arr)-1]
