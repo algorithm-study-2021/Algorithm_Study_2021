f1b854f3eed4275864d03989aa9933f3d78a9d36
leehyomin-KOR <87963124+leehyomin-KOR@users.noreply.github.com>
2021-09-25 09:47:03 +0900

    Create 6.py

---
+++ 이효민/4주차/6.py
@@ -0,0 +1,13 @@
+def solution(arr, divisor):
+    answer = []
+    
+    for i in range(0,len(arr)):
+        if arr[i]%divisor==0:
+            answer.append(arr[i])
+    
+    if len(answer)==0:
+        answer.append(-1)
+        
+    answer.sort()
+    
+    return answer
