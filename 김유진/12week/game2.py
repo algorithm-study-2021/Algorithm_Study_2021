3decea06096cf1117114e84891e62b1ec3c72593
YJ KIM <81635224+Yoojkim@users.noreply.github.com>
2021-11-19 22:36:48 +0900

    Update game2.py

--- 김유진/12week/game2.py
+++ 김유진/12week/game2.py
@@ -8,12 +8,8 @@ def solution(n,words):
         #비교
         if(words[idx] in wordPool) or (words[idx][0]!=words[idx-1][-1]):
             return [idx%n+1,idx//n+1]
-        
 
         wordPool.append(words[idx])
         idx+=1
 
     return [0,0]
-
-print(solution(2,["hello", "one", "even", "never", "now", "world", "draw"]))
-        
