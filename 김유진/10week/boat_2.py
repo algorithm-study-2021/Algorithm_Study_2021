2c73e14476e0fc6ba119a8cf57cb0b69a4a0ad82
YJ KIM <81635224+Yoojkim@users.noreply.github.com>
2021-11-20 01:34:24 +0900

    Update boat_2.py

--- 김유진/10week/boat_2.py
+++ 김유진/10week/boat_2.py
@@ -1,18 +1,14 @@
 def solution(people, limit):
     people.sort()
-    print(people)
     idx=0; idx2=len(people)-1
     out=0
 
     while 1:
         if idx==idx2: return out
         
-        print(idx,", ",idx2)
         if people[idx]+people[idx2]>limit: idx2-=1
         else: 
             if abs(idx-idx2)==1: return out+1
             else: idx+=1; idx2-=1 
 
         out+=1 #보트 개수
-
-print(solution([70, 50, 80, 50],100))
