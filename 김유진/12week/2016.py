a30cad2ed42725e84ef84552648caec7384d6ce9
YJ KIM <81635224+Yoojkim@users.noreply.github.com>
2021-11-19 22:36:22 +0900

    Update 2016.py

--- 김유진/12week/2016.py
+++ 김유진/12week/2016.py
@@ -2,12 +2,9 @@ def solution(a, b):
     day=0
     callendar=[31,29,31,30,31,30,31,31,30,31,30,31]
     for i in range(0,a-1):
-        print('돌아가나?')
         day+=callendar[i]
     day+=b
 
-    print(day)
-    #첫날 금요일(1일)
     day%=7
     if day==1: return 'FRI'
     elif day==2: return 'SAT'
@@ -16,5 +13,3 @@ def solution(a, b):
     elif day==5: return 'TUE'
     elif day==6: return 'WED'
     else: return 'THU'
-
-print(solution(1,1))
