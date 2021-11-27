2b31abda59194c783960f71b83d986ca43fe42ef
YJ KIM <81635224+Yoojkim@users.noreply.github.com>
2021-11-27 10:11:39 +0900

    Update 1_3.py

--- 김유진/13week/1_3.py
+++ 김유진/13week/1_3.py
@@ -7,7 +7,7 @@ def search(num):
         if b[i]=='0': 
             return num+2**(length-1-i)-2**(length-1-i-1) #0인거 1로 바꾸고 그 뒤 0으로 (제일 작은 거 구하기 위함 )
 
-    #모두 1인 경우는 새로운 bit 1 -> 큰 하나 0으로
+    #모두 1인 경우는 새로운 bit 1
     return num-2**(length-1)+2**length
 
 def solution(numbers):
@@ -17,5 +17,3 @@ def solution(numbers):
         else: res.append(search(i))
 
     return res
-
-print(solution([1001,337,0,1,333,673,343,221,898,997,121,1015,665,779,891,421,222,256,512,128,100]))
